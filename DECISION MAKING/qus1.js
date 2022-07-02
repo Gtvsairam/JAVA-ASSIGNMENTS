a95970b39d572d6528a8580bf406f83caf873d08
Gtv Sairam <105660096+Gtvsairam@users.noreply.github.com>
2022-07-02 19:09:19 +0530

    Add files via upload

---
+++ DECISION MAKING/qus1.js
@@ -0,0 +1,25 @@
+//QUESTION1--------
+
+//Write a program to make a simple calculator using switch statement that takes operator and two numbers from user and print the result after operation.
+
+//Note: You have to complete Calculator. No need to take any input
+
+
+const Calculator = (A, B, C) => 
+{
+    switch (A) {
+        case "+":
+            return( B + C);
+            case "-":
+            return( B - C);
+
+            case "*":
+            return( B * C);
+
+            case "/":
+            return( B / C);
+    
+        default:
+           return("No values");
+    }
+     }
